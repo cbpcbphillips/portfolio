2215eda2e11753058780e12eeaec398034a8f9ad
Vet-cg <cbpcbphillips@gmail.com>
2025-04-11 23:21:42 -0500

    Initial commit - setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
